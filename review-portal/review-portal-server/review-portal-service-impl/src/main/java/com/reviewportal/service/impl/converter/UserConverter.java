d1f0c6eb91ed5e03d3308ab5631cc6f256b0536d
selectstarfrom <this4coding@gmail.com>
2017-07-31 15:34:48 +0400

    Adds new module review-portal-service-impl. Separates service implmentation and interface into two different modules. Service interfaces, exceptions and dto's will be in service-module.

---
+++ review-portal/review-portal-server/review-portal-service-impl/src/main/java/com/reviewportal/service/impl/converter/UserConverter.java
@@ -0,0 +1,29 @@
+package com.reviewportal.service.impl.converter;
+
+import org.springframework.stereotype.Component;
+
+import com.reviewportal.model.entities.User;
+import com.reviewportal.service.dto.UserDTO;
+
+/**
+ * @author imfroz
+ *
+ */
+@Component
+public class UserConverter extends AbstractEntityTOConverter<User, UserDTO> {
+
+	@Override
+	UserDTO getNewDtoInstance() {
+		return new UserDTO();
+	}
+
+	@Override
+	User getNewEntityInstance() {
+		return new User();
+	}
+
+	public String[] getComplexTypes() {
+		return new String[] { "userRoles" };
+	}
+
+}
