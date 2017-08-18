7add4db7619142e218f19b2632ad693f1eec2f2e
selectstarfrom <this4coding@gmail.com>
2017-08-18 17:09:10 +0400

    junit

--- review-portal/review-portal-server/review-portal-service-impl/src/test/java/com/reviewportal/service/services/impl/TestUserService.java
+++ review-portal/review-portal-server/review-portal-service-impl/src/test/java/com/reviewportal/service/services/impl/TestUserService.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.reviewportal.model.entities.UserRole;
 import com.reviewportal.model.enums.UserStatus;
+import com.reviewportal.model.enums.UserType;
 import com.reviewportal.service.dto.UserDTO;
 import com.reviewportal.service.dto.UserRoleDTO;
 import com.reviewportal.service.impl.config.ServiceApplication;
@@ -35,102 +36,102 @@ import com.reviewportal.service.services.IUserServices;
 @Rollback(false)
 public class TestUserService {
 
-	static Logger LOGGER = Logger.getLogger(TestUserService.class.getName());
+    static Logger LOGGER = Logger.getLogger(TestUserService.class.getName());
 
-	@Autowired
-	private IUserServices userServices;
+    @Autowired
+    private IUserServices userServices;
 
-	@Test
-	public void testSaveUser1() throws Exception {
+    @Test
+    public void testSaveUser1() throws Exception {
 
-		String lUsername = "test_user_123";
-		String lEmail = "test_user@testmail.com";
-		String lPassword = "12345";
-		String lPhone = "223213";
-		String lPhoto = "pic_1";
-		Long lUserId = null;// 101L;
-		String lCreatedBy = "admin";
-		String lModifiedBy = "admin";
-		String lName = "user_1";
+        String lUsername = "test_user_123";
+        String lEmail = "test_user@testmail.com";
+        String lPassword = "12345";
+        String lPhone = "223213";
+        String lPhoto = "pic_1";
+        Long lUserId = null;// 101L;
+        String lCreatedBy = "admin";
+        String lModifiedBy = "admin";
+        String lName = "user_1";
 
-		Date lCreatedDate = getDateInstance();
-		Date lModifiedDate = getDateInstance();
+        Date lCreatedDate = getDateInstance();
+        Date lModifiedDate = getDateInstance();
 
-		Set<UserRoleDTO> lUserRoles = new HashSet<>();
-		lUserRoles.add(new UserRoleDTO(null, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
-				"Admin of the system"));
+        Set<UserRoleDTO> lUserRoles = new HashSet<>();
+        lUserRoles.add(new UserRoleDTO(null, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
+                "Admin of the system"));
 
-		UserDTO lUser = new UserDTO(lUserId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername,
-				lEmail, lPassword, UserStatus.ACTIVE, lPhone, lPhoto, null);
+        UserDTO lUser = new UserDTO(lUserId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername,
+                lEmail, lPassword, UserStatus.ACTIVE, lPhone, lPhoto, UserType.PROFESSIONAL, null);
 
-		LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
+        LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
 
-		userServices.save(lUser);
+        userServices.save(lUser);
 
-		List<UserDTO> lUserDto = userServices.getAll();
+        List<UserDTO> lUserDto = userServices.getAll();
 
-		LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto.get(0));
+        LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto.get(0));
 
-		assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
+        assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
 
-	}
+    }
 
-	private Date getDateInstance() {
-		return new Date();
-	}
+    private Date getDateInstance() {
+        return new Date();
+    }
 
-	@Test
-	public void testSaveUser2() throws Exception {
+    @Test
+    public void testSaveUser2() throws Exception {
 
-		String lUsername1 = "test_user_1";
-		String lEmail1 = "test_user1@testmail.com";
-		String lPassword1 = "pass111";
-		String lPhone1 = "1111";
-		String lPhoto1 = "pic_1";
-		Long lId1 = 101L;
-		String lCreatedBy1 = "admin";
-		String lModifiedBy1 = "admin";
-		String lName1 = "user_1";
+        String lUsername1 = "test_user_1";
+        String lEmail1 = "test_user1@testmail.com";
+        String lPassword1 = "pass111";
+        String lPhone1 = "1111";
+        String lPhoto1 = "pic_1";
+        Long lId1 = 101L;
+        String lCreatedBy1 = "admin";
+        String lModifiedBy1 = "admin";
+        String lName1 = "user_1";
 
-		Date lCreatedDate1 = getDateInstance();
-		Date lModifiedDate1 = getDateInstance();
+        Date lCreatedDate1 = getDateInstance();
+        Date lModifiedDate1 = getDateInstance();
 
-		Set<UserRole> lUserRoles1 = new HashSet<>();
-		lUserRoles1.add(new UserRole(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, "ADMIN",
-				"Admin of the system"));
+        Set<UserRole> lUserRoles1 = new HashSet<>();
+        lUserRoles1.add(new UserRole(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, "ADMIN",
+                "Admin of the system"));
 
-		UserDTO lUser1 = new UserDTO(null, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, lName1, lUsername1,
-				lEmail1, lPassword1, UserStatus.ACTIVE, lPhone1, lPhoto1, null);
+        UserDTO lUser1 = new UserDTO(null, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, lName1, lUsername1,
+                lEmail1, lPassword1, UserStatus.ACTIVE, lPhone1, lPhoto1, UserType.PROFESSIONAL, null);
 
-		String lUsername2 = "test_user_2";
-		String lEmail2 = "test_user2@testmail.com";
-		String lPassword2 = "pass222";
-		String lPhone2 = "2222";
-		String lPhoto2 = "pic_2";
-		Long lId2 = 101L;
-		String lCreatedBy2 = "admin2";
-		String lModifiedBy2 = "admin2";
-		String lName2 = "user_2";
+        String lUsername2 = "test_user_2";
+        String lEmail2 = "test_user2@testmail.com";
+        String lPassword2 = "pass222";
+        String lPhone2 = "2222";
+        String lPhoto2 = "pic_2";
+        Long lId2 = 101L;
+        String lCreatedBy2 = "admin2";
+        String lModifiedBy2 = "admin2";
+        String lName2 = "user_2";
 
-		Date lCreatedDate2 = getDateInstance();
-		Date lModifiedDate2 = getDateInstance();
+        Date lCreatedDate2 = getDateInstance();
+        Date lModifiedDate2 = getDateInstance();
 
-		Set<UserRole> lUserRoles2 = new HashSet<>();
-		lUserRoles2.add(new UserRole(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, "ADMIN",
-				"Admin of the system"));
+        Set<UserRole> lUserRoles2 = new HashSet<>();
+        lUserRoles2.add(new UserRole(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, "ADMIN",
+                "Admin of the system"));
 
-		UserDTO lUser2 = new UserDTO(null, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, lName2, lUsername2,
-				lEmail2, lPassword2, UserStatus.ACTIVE, lPhone2, lPhoto2, null);
+        UserDTO lUser2 = new UserDTO(null, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, lName2, lUsername2,
+                lEmail2, lPassword2, UserStatus.ACTIVE, lPhone2, lPhoto2, UserType.PROFESSIONAL, null);
 
-		List<UserDTO> lUsers = Arrays.asList(lUser1, lUser2);
-		LOGGER.debug("testUserEntityToUserTO List: User Entity 1: " + lUser1);
-		LOGGER.debug("testUserEntityToUserTO List: User Entity 2: " + lUser2);
+        List<UserDTO> lUsers = Arrays.asList(lUser1, lUser2);
+        LOGGER.debug("testUserEntityToUserTO List: User Entity 1: " + lUser1);
+        LOGGER.debug("testUserEntityToUserTO List: User Entity 2: " + lUser2);
 
-		userServices.save(lUsers);
+        userServices.save(lUsers);
 
-		List<UserDTO> lAll = userServices.getAll();
+        List<UserDTO> lAll = userServices.getAll();
 
-		assertTrue("testSaveUser2 Failed", !lAll.isEmpty());
+        assertTrue("testSaveUser2 Failed", !lAll.isEmpty());
 
-	}
+    }
 }
--- review-portal/review-portal-server/review-portal-service-impl/src/test/java/com/reviewportal/service/test/converter/TestUserMapper.java
+++ review-portal/review-portal-server/review-portal-service-impl/src/test/java/com/reviewportal/service/test/converter/TestUserMapper.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.reviewportal.model.entities.User;
 import com.reviewportal.model.entities.UserRole;
 import com.reviewportal.model.enums.UserStatus;
+import com.reviewportal.model.enums.UserType;
 import com.reviewportal.service.dto.UserDTO;
 import com.reviewportal.service.dto.UserRoleDTO;
 import com.reviewportal.service.impl.config.ServiceApplication;
@@ -31,150 +32,150 @@ import com.reviewportal.service.impl.converter.UserConverter;
 @SpringBootTest(classes = ServiceApplication.class)
 public class TestUserMapper {
 
-	@Autowired
-	private UserConverter converter;
+    @Autowired
+    private UserConverter converter;
 
-	static Logger LOGGER = Logger.getLogger(TestUserMapper.class.getName());
+    static Logger LOGGER = Logger.getLogger(TestUserMapper.class.getName());
 
-	/**
-	 * Test for converting User Entity to User TO
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserEntityToUserTO0() throws Exception {
+    /**
+     * Test for converting User Entity to User TO
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testUserEntityToUserTO0() throws Exception {
 
-		String lEmail = "test_user@testmail.com";
-		String lUsername = "test_user";
-		String lPassword = "12345";
-		String lPhone = "223213";
-		String lPhoto = "pic_1";
-		Long lId = 101L;
-		String lCreatedBy = "admin";
-		String lModifiedBy = "admin";
-		String lName = "user_1";
+        String lEmail = "test_user@testmail.com";
+        String lUsername = "test_user";
+        String lPassword = "12345";
+        String lPhone = "223213";
+        String lPhoto = "pic_1";
+        Long lId = 101L;
+        String lCreatedBy = "admin";
+        String lModifiedBy = "admin";
+        String lName = "user_1";
 
-		Date lCreatedDate = getDateInstance();
-		Date lModifiedDate = getDateInstance();
+        Date lCreatedDate = getDateInstance();
+        Date lModifiedDate = getDateInstance();
 
-		Set<UserRoleDTO> lUserRoles = new HashSet<>();
-		UserRoleDTO lUserRoleDTO = new UserRoleDTO(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
-				"Admin of the system");
-		lUserRoles.add(lUserRoleDTO);
+        Set<UserRoleDTO> lUserRoles = new HashSet<>();
+        UserRoleDTO lUserRoleDTO = new UserRoleDTO(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
+                "Admin of the system");
+        lUserRoles.add(lUserRoleDTO);
 
-		UserDTO lUser = new UserDTO(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername, lEmail,
-				lPassword, UserStatus.ACTIVE, lPhone, lPhoto, lUserRoles);
+        UserDTO lUser = new UserDTO(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername, lEmail,
+                lPassword, UserStatus.ACTIVE, lPhone, lPhoto, UserType.PROFESSIONAL, lUserRoles);
 
-		LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
+        LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
 
-		User lUserDto = converter.getEnity(lUser);
+        User lUserDto = converter.getEnity(lUser);
 
-		LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto);
+        LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto);
 
-		assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
+        assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
 
-	}
+    }
 
-	/**
-	 * Test for converting User Entity to User TO
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserEntityToUserTO1() throws Exception {
+    /**
+     * Test for converting User Entity to User TO
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testUserEntityToUserTO1() throws Exception {
 
-		String lUsername = "test_user";
-		String lEmail = "test_user@testmail.com";
-		String lPassword = "12345";
-		String lPhone = "223213";
-		String lPhoto = "pic_1";
-		Long lId = 101L;
-		String lCreatedBy = "admin";
-		String lModifiedBy = "admin";
-		String lName = "user_1";
+        String lUsername = "test_user";
+        String lEmail = "test_user@testmail.com";
+        String lPassword = "12345";
+        String lPhone = "223213";
+        String lPhoto = "pic_1";
+        Long lId = 101L;
+        String lCreatedBy = "admin";
+        String lModifiedBy = "admin";
+        String lName = "user_1";
 
-		Date lCreatedDate = getDateInstance();
-		Date lModifiedDate = getDateInstance();
+        Date lCreatedDate = getDateInstance();
+        Date lModifiedDate = getDateInstance();
 
-		Set<UserRole> lUserRoles = new HashSet<>();
-		lUserRoles.add(new UserRole(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
-				"Admin of the system"));
+        Set<UserRole> lUserRoles = new HashSet<>();
+        lUserRoles.add(new UserRole(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, "ADMIN",
+                "Admin of the system"));
 
-		User lUser = new User(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername, lEmail,
-				lPassword, UserStatus.ACTIVE, lPhone, lPhoto, lUserRoles);
+        User lUser = new User(lId, lCreatedDate, lCreatedBy, lModifiedDate, lModifiedBy, lName, lUsername, lEmail,
+                lPassword, UserStatus.ACTIVE, lPhone, lPhoto, UserType.PROFESSIONAL, lUserRoles);
 
-		LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
+        LOGGER.debug("testUserEntityToUserTO: User: " + lUser);
 
-		UserConverter lConverter = new UserConverter();
+        UserConverter lConverter = new UserConverter();
 
-		UserDTO lUserDto = lConverter.getDto(lUser);
+        UserDTO lUserDto = lConverter.getDto(lUser);
 
-		LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto);
+        LOGGER.debug("testUserEntityToUserTO: UserTO: " + lUserDto);
 
-		assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
+        assertEquals("User Entity to User DTO Convertion Failed", lUser.getId(), lUser.getId());
 
-	}
+    }
 
-	private Date getDateInstance() {
-		return new Date();
-	}
+    private Date getDateInstance() {
+        return new Date();
+    }
 
-	@Test
-	public void testUserEntityToUserTOList() throws Exception {
+    @Test
+    public void testUserEntityToUserTOList() throws Exception {
 
-		String lUsername1 = "test_user_1";
-		String lEmail1 = "test_user1@testmail.com";
-		String lPassword1 = "pass111";
-		String lPhone1 = "1111";
-		String lPhoto1 = "pic_1";
-		Long lId1 = 101L;
-		String lCreatedBy1 = "admin";
-		String lModifiedBy1 = "admin";
-		String lName1 = "user_1";
+        String lUsername1 = "test_user_1";
+        String lEmail1 = "test_user1@testmail.com";
+        String lPassword1 = "pass111";
+        String lPhone1 = "1111";
+        String lPhoto1 = "pic_1";
+        Long lId1 = 101L;
+        String lCreatedBy1 = "admin";
+        String lModifiedBy1 = "admin";
+        String lName1 = "user_1";
 
-		Date lCreatedDate1 = getDateInstance();
-		Date lModifiedDate1 = getDateInstance();
+        Date lCreatedDate1 = getDateInstance();
+        Date lModifiedDate1 = getDateInstance();
 
-		Set<UserRole> lUserRoles1 = new HashSet<>();
-		lUserRoles1.add(new UserRole(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, "ADMIN",
-				"Admin of the system"));
+        Set<UserRole> lUserRoles1 = new HashSet<>();
+        lUserRoles1.add(new UserRole(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, "ADMIN",
+                "Admin of the system"));
 
-		User lUser1 = new User(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, lName1, lUsername1,
-				lEmail1, lPassword1, UserStatus.ACTIVE, lPhone1, lPhoto1, lUserRoles1);
+        User lUser1 = new User(lId1, lCreatedDate1, lCreatedBy1, lModifiedDate1, lModifiedBy1, lName1, lUsername1,
+                lEmail1, lPassword1, UserStatus.ACTIVE, lPhone1, lPhoto1, UserType.PROFESSIONAL, lUserRoles1);
 
-		String lEmail2 = "test_user2@testmail.com";
-		String lUsername2 = "test_user_1";
-		String lPassword2 = "pass222";
-		String lPhone2 = "2222";
-		String lPhoto2 = "pic_2";
-		Long lId2 = 101L;
-		String lCreatedBy2 = "admin2";
-		String lModifiedBy2 = "admin2";
-		String lName2 = "user_2";
+        String lEmail2 = "test_user2@testmail.com";
+        String lUsername2 = "test_user_1";
+        String lPassword2 = "pass222";
+        String lPhone2 = "2222";
+        String lPhoto2 = "pic_2";
+        Long lId2 = 101L;
+        String lCreatedBy2 = "admin2";
+        String lModifiedBy2 = "admin2";
+        String lName2 = "user_2";
 
-		Date lCreatedDate2 = getDateInstance();
-		Date lModifiedDate2 = getDateInstance();
+        Date lCreatedDate2 = getDateInstance();
+        Date lModifiedDate2 = getDateInstance();
 
-		Set<UserRole> lUserRoles2 = new HashSet<>();
-		lUserRoles2.add(new UserRole(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, "ADMIN",
-				"Admin of the system"));
+        Set<UserRole> lUserRoles2 = new HashSet<>();
+        lUserRoles2.add(new UserRole(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, "ADMIN",
+                "Admin of the system"));
 
-		User lUser2 = new User(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, lName2, lUsername2,
-				lEmail2, lPassword2, UserStatus.ACTIVE, lPhone2, lPhoto2, lUserRoles2);
+        User lUser2 = new User(lId2, lCreatedDate2, lCreatedBy2, lModifiedDate2, lModifiedBy2, lName2, lUsername2,
+                lEmail2, lPassword2, UserStatus.ACTIVE, lPhone2, lPhoto2, UserType.PROFESSIONAL, lUserRoles2);
 
-		List<User> lUsers = Arrays.asList(lUser1, lUser2);
-		LOGGER.debug("testUserEntityToUserTO List: User Entity 1: " + lUser1);
-		LOGGER.debug("testUserEntityToUserTO List: User Entity 2: " + lUser2);
+        List<User> lUsers = Arrays.asList(lUser1, lUser2);
+        LOGGER.debug("testUserEntityToUserTO List: User Entity 1: " + lUser1);
+        LOGGER.debug("testUserEntityToUserTO List: User Entity 2: " + lUser2);
 
-		UserConverter lConverter = new UserConverter();
+        UserConverter lConverter = new UserConverter();
 
-		List<UserDTO> lUserDto = lConverter.getDtos(lUsers);
+        List<UserDTO> lUserDto = lConverter.getDtos(lUsers);
 
-		LOGGER.debug("testUserEntityToUserTO List: User TO 1: " + lUserDto.get(0));
-		LOGGER.debug("testUserEntityToUserTO List: User TO 2: " + lUserDto.get(1));
+        LOGGER.debug("testUserEntityToUserTO List: User TO 1: " + lUserDto.get(0));
+        LOGGER.debug("testUserEntityToUserTO List: User TO 2: " + lUserDto.get(1));
 
-		assertEquals("User Entity to User DTO List Convertion Failed", lUser1.getId(), lUserDto.get(0).getId());
-		assertEquals("User Entity to User DTO List Convertion Failed", lUser2.getId(), lUserDto.get(1).getId());
+        assertEquals("User Entity to User DTO List Convertion Failed", lUser1.getId(), lUserDto.get(0).getId());
+        assertEquals("User Entity to User DTO List Convertion Failed", lUser2.getId(), lUserDto.get(1).getId());
 
-	}
+    }
 }
