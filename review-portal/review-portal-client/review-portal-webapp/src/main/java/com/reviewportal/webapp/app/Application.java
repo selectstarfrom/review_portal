184f29a7d0fad1a96244754ced58567a314294f9
selectstarfrom <this4coding@gmail.com>
2017-09-06 21:15:13 +0400

    ununsed import

--- review-portal/review-portal-client/review-portal-webapp/src/main/java/com/reviewportal/webapp/app/Application.java
+++ review-portal/review-portal-client/review-portal-webapp/src/main/java/com/reviewportal/webapp/app/Application.java
@@ -3,21 +3,14 @@ package com.reviewportal.webapp.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.UrlTemplateResolver;
 
 import com.reviewportal.model.core.IAppLauncher;
 import com.reviewportal.service.impl.config.ServiceApplication;
-
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 // @formatter:off
 @SpringBootApplication
@@ -34,6 +27,5 @@ public class Application {
     public static void main(String[] pArgs) {
         SpringApplication.run(Application.class, pArgs);
     }
-    
 
 }
